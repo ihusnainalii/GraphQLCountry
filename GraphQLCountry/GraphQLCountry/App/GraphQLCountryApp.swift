6afb8989d7ad200b4db4b8be376b287395337044
Husnain Ali <ali@ili.digital>
2024-07-19 17:11:24 +0500

    Project configure and install dependencies
    Project configure and install dependencies

---
+++ GraphQLCountry/GraphQLCountry/App/GraphQLCountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLCountryApp.swift
+//  GraphQLCountry
+//
+//  Created by Husnain Ali - ILI on 19/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLCountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GraphQLCountry/GraphQLCountry/Frontend/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GraphQLCountry
+//
+//  Created by Husnain Ali - ILI on 19/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
